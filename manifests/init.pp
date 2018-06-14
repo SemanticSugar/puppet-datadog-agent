f021616afa22bc91d0d1cf01ba10178f60f3c56b
Bill Medernach <bill.medernach@adroll.com>
2018-06-14 10:17:02 -0400

    Removed unnecessary code

--- manifests/init.pp
+++ manifests/init.pp
@@ -520,17 +520,6 @@ class datadog_agent(
     }
   }
 
-  # specific for dotcom webservers
-  file { '/etc/dd-agent/datadog.conf':
-    ensure  => file,
-    content => template('datadog_agent/datadog.conf.erb'),
-    owner   => $datadog_agent::params::dd_user,
-    group   => $datadog_agent::params::dd_group,
-    mode    => '0640',
-    notify  => Service[$datadog_agent::params::service_name],
-    require => File['/etc/dd-agent'],
-  }
-
   if $puppet_run_reports {
     class { 'datadog_agent::reports':
       api_key                   => $api_key,
